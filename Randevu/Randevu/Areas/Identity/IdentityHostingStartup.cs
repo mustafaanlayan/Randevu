76a81686bdb3c1ad78a3a20008ca4b635e4456ef
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-12 11:55:46 +0300

    commit kayıt ol 2

---
+++ Randevu/Randevu/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Randevu.Data;
+using Randevu.Data.Entity;
+
+[assembly: HostingStartup(typeof(Randevu.Areas.Identity.IdentityHostingStartup))]
+namespace Randevu.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
